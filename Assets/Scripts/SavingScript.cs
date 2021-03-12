d505ad8692456825371f87132e4fe020ffd69b11
Laura <lamangel@my.centennialcollege.ca>
2021-03-12 16:05:08 +0600

    saving scripts + firePlace  (but i dont think we will use it)

---
+++ Assets/Scripts/SavingScript.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavingScript
+{
+    [System.Serializable]
+    public class SavingScript 
+    {
+        public int level;
+        public int health;
+       /* public SavingScript(Player player)
+        {
+            level = player.level;
+            health = player.health;
+
+        }*/
+       //we have to have the script for the player to be able to save this thing, bcs rn it says that there is no 'player' script in it and thats an error
+    }
+
+
+}
