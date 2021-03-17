867b92c94624b74090a08497a29e46fa8d080a8f
lamonikim <77967939+lamonikim@users.noreply.github.com>
2021-03-17 13:38:03 +0600

    Update randomMovements.cs

--- Assets/Scripts/randomMovements.cs
+++ Assets/Scripts/randomMovements.cs
@@ -4,40 +4,7 @@ using UnityEngine;
 
 public class randomMovements : MonoBehaviour
 {
-    private float timeBtwAttack;
-    public float startTimeBtwAttack;
-    public Transform attackPos;
-    public float attackRange;
-    public LayerMask whatIsEnemies;
-    public int damage;
-    void Update()
-    {
-        if (timeBtwAttack <= 0)
-        {
-            if (Input.GetKey(KeyCode.Space))
-            {
-                //we can use another button 
-                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
-                for (int i = 0; i < enemiesToDamage.Length; i++)
-                {
-                    //enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
-                    // if we want to make damage we have to write this script into the Slowman 
-                    /* public void TakeDamage(int damage){
-                       health -= damage;  (ps slowman must have 'public int health' in his script)
-                       Debug.Log("Damage taken")
-                     */
-                }
-            }
-            timeBtwAttack = startTimeBtwAttack;
-        } else
-        {
-            timeBtwAttack -= Time.deltaTime;
-        }
-    }
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+   
     }
 
 
