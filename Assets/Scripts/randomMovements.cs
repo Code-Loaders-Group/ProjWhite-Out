fe746402c6e63412ac9bd0c39995de61d120b4ee
lamonikim <77967939+lamonikim@users.noreply.github.com>
2021-03-17 16:12:21 +0600

    I made a script but we have to test
    my unity is lagging again :_)

--- Assets/Scripts/randomMovements.cs
+++ Assets/Scripts/randomMovements.cs
@@ -4,7 +4,43 @@ using UnityEngine;
 
 public class randomMovements : MonoBehaviour
 {
+   public float moveSpeed = 3f;
+   Transform leftWayPoint, rightWayPoint;
+   Vector3 localScale;
+   bool movingRight = true;
+   RigidBody2D rb;
    
-   
+ void Start () {
+ localScale = transform.localScale;
+ rb = GetComponent<RigidBody2D>();
+ leftWayPoint = GameObject.Find("LeftWayPoint").GetComponent<Transform>();
+ rightWayPoint = GameObject.Find("RightWayPoint").GetComponent<Transform>();
+ 
+ }
+ void Update () {
+ if (transform.position.x > rightWayPosition.position.x)
+ movingRight = false;
+ if (transform.position.x < leftWayPosition.position.x)
+ movingRight = true;
+ 
+ if (movingRight)
+ moveRight();
+ else 
+ moveLeft();
+ }
+ void moveRight () {
+ movingRight = true;
+ localScale.x = 1;
+ transfrom.localScale = localScale;
+ rb.velocity = new Vector2 (localScale.x * moveSpeed, rb.velocity.y);
+ }
+ 
+ void moveLeft () {
+ movingRight = false;
+ localScale.x = -1;
+ transfrom.localScale = localScale;
+ rb.velocity = new Vector2 (localScale.x * moveSpeed, rb.velocity.y);
+ }
+ 
  }
 
