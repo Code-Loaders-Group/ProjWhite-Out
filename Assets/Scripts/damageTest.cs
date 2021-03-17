40714db25f4bba83bdfc8d4eb812931f7b5c3d5e
lamonikim <77967939+lamonikim@users.noreply.github.com>
2021-03-17 13:27:38 +0600

    Update damageTest.cs
    here is the script for the damage mechanics

--- Assets/Scripts/damageTest.cs
+++ Assets/Scripts/damageTest.cs
@@ -3,16 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class damageTest : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+{
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+    public Transform attackPos;
+    public float attackRange;
+    public LayerMask whatIsEnemies;
+    public int damage;
     void Update()
     {
-        
+        if (timeBtwAttack <= 0)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                //we can use another button 
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                    // if we want to make damage we have to write this script into the Slowman 
+                    /* public void TakeDamage(int damage){
+                       health -= damage;  (ps slowman must have 'public int health' in his script)
+                       Debug.Log("Damage taken")
+                     */
+                }
+            }
+            timeBtwAttack = startTimeBtwAttack;
+        } else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
     }
-}
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+
+
+    }
+
