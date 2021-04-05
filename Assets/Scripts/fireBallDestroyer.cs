03ab7488f7ff7c5e7133e74104b6b417677e53da
Alex M <amaynar4@my.centennialcollege.ca>
2021-04-04 22:14:24 -0500

    Revert "added fireball aiming and enemy explosion"
    This reverts commit d9881238f89e44c162e6fd5fed4b9bb00f3933d2.

--- Assets/Scripts/fireBallDestroyer.cs
+++ Assets/Scripts/fireBallDestroyer.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class fireBallDestroyer : MonoBehaviour
 {
-    public GameObject enemy_explosion;
-
-    private void Start()
-    {
-        Physics2D.IgnoreLayerCollision(6, 7);
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
 
@@ -18,7 +11,6 @@ public class fireBallDestroyer : MonoBehaviour
         {
             Destroy(collision.gameObject);
             Destroy(this.gameObject);
-            Instantiate(enemy_explosion, transform.position, transform.rotation);
         }
         else Destroy(this.gameObject);
     }
--- Assets/Scripts/fireBallMover.cs
+++ Assets/Scripts/fireBallMover.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class fireBallMover : MonoBehaviour
 {
     public float speed = 10f;
-    public Rigidbody2D rb;
 
+    public float lifespan = 3f;
 
     void Start()
     {
-
-        rb.velocity = transform.right * speed;
-        
-        //GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0.0f);      
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0.0f);
+        Destroy(gameObject, lifespan);
     }
 }
