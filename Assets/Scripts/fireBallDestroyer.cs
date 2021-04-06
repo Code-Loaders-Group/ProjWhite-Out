e6f869e6577650ffa18b07d70ee24d5da917dd2b
Alex M <amaynar4@my.centennialcollege.ca>
2021-04-06 16:14:21 -0500

    Fixed fireball aiming, added fireball explosion with sound for slowmen

--- Assets/Scripts/fireBallDestroyer.cs
+++ Assets/Scripts/fireBallDestroyer.cs
@@ -4,24 +4,20 @@ using UnityEngine;
 
 public class fireBallDestroyer : MonoBehaviour
 {
-
-    public void Start()
+    public GameObject explosion;
+    public void Update()
     {
-        
+        Physics2D.IgnoreLayerCollision(6, 7);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
 
-        if (collision.gameObject.tag == "Player")
-        {
-            Physics2D.IgnoreLayerCollision(6, 7);
-        }
-
         if (collision.gameObject.tag == "enemy")
         {
             Destroy(collision.gameObject);
             Destroy(this.gameObject);
+            Instantiate(explosion, collision.transform.position, collision.transform.rotation);
         }
         else Destroy(this.gameObject);
     }
