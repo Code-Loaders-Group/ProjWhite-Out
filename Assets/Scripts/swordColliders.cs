8d2d7fe46fa6e711b3add9f542ac7d2474522543
Alex M <amaynar4@my.centennialcollege.ca>
2021-04-07 14:14:09 -0500

    Implemented initial sword attack

---
+++ Assets/Scripts/swordColliders.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swordColliders : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "enemy")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
