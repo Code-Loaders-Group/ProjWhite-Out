8e955aa579303596b585988c9218693af28af605
Alex M <amaynar4@my.centennialcollege.ca>
2021-04-08 13:50:48 -0500

    Fixed weird slowmen spinning movement

--- Assets/Scripts/randomMovements.cs
+++ Assets/Scripts/randomMovements.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 public class randomMovements : MonoBehaviour
 {
-  public Transform target;
-     public float speed = 3f;
- 
- 
-     void Start () {
-         
-     }
- 
-     void Update(){
-         
-         
-         transform.LookAt(target.position);
-         transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+    // Assigns the end position to the player
+    public Transform target;
+    public float speed = 3f;
 
 
+    public void Start()
+    {
+        
+    }
 
-        if (Vector3.Distance(transform.position,target.position)>1f){
-             transform.Translate(new Vector3(speed* Time.deltaTime,0,0) );
-         }
+    void Update()
+     {
+        //Moves the enemy towards the player using only Vector2, this way we don't get the weird spinning like before
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        
+        
+        //old enemy mover script to follow player
+
+         //transform.LookAt(target.position);
+         //transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+
+        //if (Vector3.Distance(transform.position,target.position)>1f){
+        //     transform.Translate(new Vector3(speed* Time.deltaTime,0,0) );
+        //}
  
      }
  
