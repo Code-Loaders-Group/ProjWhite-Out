8cbfdfc756439afa4aefaefa725e1a8c140ac7c1
Alex M <amaynar4@my.centennialcollege.ca>
2021-04-08 20:31:35 -0500

    added explosions if fireball hits anything,  fixed the enemy collisions

--- Assets/Scripts/fireBallDestroyer.cs
+++ Assets/Scripts/fireBallDestroyer.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class fireBallDestroyer : MonoBehaviour
 {
+    public GameObject fireBall;
+
+
     public GameObject explosion;
-    public void Update()
+
+    public void Start()
     {
         Physics2D.IgnoreLayerCollision(6, 7);
+        // To ignore the invisible spawn triggers (It's very buggy)
+        Physics2D.IgnoreLayerCollision(6, 11);
     }
 
+
+
+ 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
+
 
         if (collision.gameObject.tag == "enemy")
         {
@@ -19,6 +30,17 @@ public class fireBallDestroyer : MonoBehaviour
             Destroy(this.gameObject);
             Instantiate(explosion, collision.transform.position, collision.transform.rotation);
         }
-        else Destroy(this.gameObject);
+        // this ensure that the fireball explodes when it hits the walls, but it does not destroy the walls
+        else if (collision.gameObject.tag == "walls" || collision.gameObject.tag == "bossDoor")
+        {
+            Destroy(this.gameObject);
+            Instantiate(explosion, fireBall.transform.position, fireBall.transform.rotation);
+        }
+        else
+        {
+            // for fires, spawners and anything that is not an enemy
+            Destroy(this.gameObject);
+            Instantiate(explosion, fireBall.transform.position, fireBall.transform.rotation);
+        }
     }
 }
